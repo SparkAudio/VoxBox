1d4c3e5e6f9ab29000d83e2338069822edd348e2
spark-tts <846379560@qq.com>
2025-05-05 12:44:03 +0800

    add description for annotation

--- tools/utils/audio.py
+++ tools/utils/audio.py
@@ -1,5 +1,5 @@
-# Copyright (c) 2024 WaveVortex
-#               2024 Xinsheng Wang (w.xinshawn@gmail.com)
+# Copyright (c) 2025 SparkAudio
+#               2025 Xinsheng Wang (w.xinshawn@gmail.com)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
